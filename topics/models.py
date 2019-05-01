ecbea6530e160652558d84f9f57629b1d5a63277
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-05-01 18:27:04 +0900

    Create Topic & Comment Model

--- topics/models.py
+++ topics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Topic(models.Model):
+    tittle_text = models.CharField(max_length=50)
+    content_text = models.CharField(max_length=140)
+
+class Comment(models.Model):
+    content_text = models.CharField(max_length=140)
